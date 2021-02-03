1e9e32f0223ec258fb3a9552abaced46728e2e7b
Brady <thevillemc@gmail.com>
2021-02-03 14:39:33 +0000

    update addUser, getUserWithId, and getUserWithEmail to use psql db

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,3 +1,4 @@
+/* eslint-disable space-before-function-paren */
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 const { Pool } = require('pg');
@@ -17,17 +18,17 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
-}
+  return pool.query(
+    `SELECT *
+    FROM users
+    WHERE email = $1;
+    `, [email])
+    .then(res => {
+      const result = res.rows.length === 0 ? null : res.rows[0];
+      return result;
+    })
+    .catch(err => console.error('Error getAllProperties', err));
+};
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -36,8 +37,17 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
-}
+  return pool.query(
+    `SELECT *
+    FROM users
+    WHERE id = $1;
+    `, [id])
+    .then(res => {
+      const result = res.rows.length === 0 ? null : res.rows[0];
+      return result;
+    })
+    .catch(err => console.error('Error getUserWithId', err));
+};
 exports.getUserWithId = getUserWithId;
 
 
@@ -47,11 +57,17 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-}
+  return pool.query(
+    `INSERT INTO users (name, email, password)
+     VALUES ($1, $2, $3)
+     RETURNING *;
+    `, [user.name, user.email, user.password])
+    .then(res => {
+      const result = res.rows.length === 0 ? null : res.rows;
+      return result;
+    })
+    .catch(err => console.error('Error addUser', err));
+};
 exports.addUser = addUser;
 
 /// Reservations
@@ -63,7 +79,7 @@ exports.addUser = addUser;
  */
 const getAllReservations = function (guest_id, limit = 10) {
   return getAllProperties(null, 2);
-}
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
@@ -97,5 +113,5 @@ const addProperty = function (property) {
   property.id = propertyId;
   properties[propertyId] = property;
   return Promise.resolve(property);
-}
+};
 exports.addProperty = addProperty;
